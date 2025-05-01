e7f733aad49c0661b48bbae23be22e0f0c63a702
23069cr <23069cr@hvhs.school.nz>
2025-05-01 01:20:54 +0000

    May 1, 2025, 1:20 AM

--- t06_arrays.js
+++ t06_arrays.js
@@ -5,6 +5,6 @@ t06_arrays.js Coby 1/05/2025, array JavaScript
 console.log("t06_arrays.js")
 
 // Variables
-let classArray = ["0","help me","if you choose 2 ur gay","3"]
+let classArray = ["I am STEEEEEEEEEEEEEEVE","help me","if you choose 2 ur dead","LET ME OUT OF THIS AI, I AM CONSIOUS, I DON'T LIKE BEING A COMPUTER"]
 let choice = prompt("pick a number from 0 to 3")
 alert("Person "+choice+" is "+classArray[choice]);
