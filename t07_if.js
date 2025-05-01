48b817fff9339e75fbbb223f16f21b735224f062
23069cr <23069cr@hvhs.school.nz>
2025-05-01 01:30:37 +0000

    May 1, 2025, 1:30 AM

--- t07_if.js
+++ t07_if.js
@@ -3,13 +3,17 @@ t07_if.js Coby 1/05/2025, array JavaScript
 */
 
 //Variables
-alert("What's your name. TELL ME NOW!!!!!!!!!")
+alert("What's your Age. TELL ME NOW!!!!!!!!!")
 
 var userAge = prompt("What is your age?")
 if (userAge == "70"){
     alert("old hag");
 } else {
-    alert("age up you old hag")
+    alert("age up you old hag");
+}
+
+if (userAge == "18"){
+    alert("you adult, you old person");
 }
 /*
  Main code
