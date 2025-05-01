23a4df65f3a56f79a076806e6978cefcb6e410ee
23069cr <23069cr@hvhs.school.nz>
2025-05-01 01:02:28 +0000

    May 1, 2025, 1:02 AM

---
+++ t05_alerts_prompts.js
@@ -0,0 +1,11 @@
+/*
+t05_alerts_prompts.js Coby 1/05/2025, alerts and prompts JavaScript
+*/
+console.log("do it")
+
+/*
+ Main code
+ */
+alert("What's your name. TELL ME NOW!!!!!!!!!")
+
+var userName = prompt("what is your name?", "Bob")
