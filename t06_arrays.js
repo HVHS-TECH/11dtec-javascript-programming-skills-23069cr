7ee6ad41ddca42c044b5ab1bc0200ac02532a807
23069cr <23069cr@hvhs.school.nz>
2025-05-01 01:17:42 +0000

    May 1, 2025, 1:17 AM

--- t06_arrays.js
+++ t06_arrays.js
@@ -3,3 +3,8 @@ t06_arrays.js Coby 1/05/2025, array JavaScript
 */
 
 console.log("t06_arrays.js")
+
+// Variables
+let classArray = ["0","1","2","3"]
+let choice = prompt("pick a number from 0 to 3")
+alert("Person "+choice+" is "+classArray[cjoice]);
