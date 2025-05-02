f4941e9b8483d3ba521613ce3ec51dc4bf228749
23069cr <23069cr@hvhs.school.nz>
2025-05-02 02:31:04 +0000

    May 2, 2025, 2:31 AM

--- t07_if.js
+++ t07_if.js
@@ -4,7 +4,7 @@ t07_if.js Coby 1/05/2025, array JavaScript
 
 //Variables
 alert("What's your Age. TELL ME NOW!!!!!!!!!")
-
+var name
 var userAge = prompt("What is your age?")
 if (userAge > "70"){
     alert("You're practically dust");
@@ -17,6 +17,9 @@ if (userAge == "18"){
     alert("you adult, you old person");
 }
 
+prompt("What is your name?")
+
+prompt("hello " +name+ "welcome")
 /*
  Main code
  */
