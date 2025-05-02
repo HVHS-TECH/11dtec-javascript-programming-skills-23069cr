dfb2e2a5cf2e4e263e51294ca1f638712a7c4efe
23069cr <23069cr@hvhs.school.nz>
2025-05-02 02:44:06 +0000

    May 2, 2025, 2:44 AM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -3,8 +3,9 @@ t07_if.js Coby 1/05/2025, array JavaScript
 */
 
 //Variables
-for (count = 1; count< 20; count++){
-    
+console.log("starting count")
+for (count = 1; count < 20; count++) {
+    console.log("Type: " + count);
 }
 
 
