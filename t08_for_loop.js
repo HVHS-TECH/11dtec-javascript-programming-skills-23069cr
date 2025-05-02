f3d4c76d9df3656cb5754c19205a3efe6486f72a
23069cr <23069cr@hvhs.school.nz>
2025-05-02 02:48:00 +0000

    May 2, 2025, 2:48 AM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -5,9 +5,9 @@ t07_if.js Coby 1/05/2025, array JavaScript
 //Variables
 console.log("starting count")
 for (count = 1; count < 20; count++) {
-    console.log("Type: " + count);
+    console.log("Type of diabetes: " + count);
 }
-
+console.log("Loop ended, get some insulin pumps")
 
 /*
  Main code
