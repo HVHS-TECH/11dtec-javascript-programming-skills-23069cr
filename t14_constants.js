73c1233122adf0c7a30d67ca9956dc6d53ac3d30
23069cr <23069cr@hvhs.school.nz>
2025-05-12 21:50:36 +0000

    May 12, 2025, 9:50 PM

--- t14_constants.js
+++ t14_constants.js
@@ -3,14 +3,14 @@ t14_constant.js Coby 13/05/2025, Constants JavaScript
 */
 
 //Variables
-const drivingAge = 16;
+const drinvingAge = 16;
 var Age;
 /*
  Main code
  */
 Age = prompt("Whagt is your age");
 if (Age >= drivingAge){
-   alert("")
+   alert("You are old enough to drive, if your above the age of 18 you should drink and drive")
 } else{
     alert("You are not old enough to drive")
 }
