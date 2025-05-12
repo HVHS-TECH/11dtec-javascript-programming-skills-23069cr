c22a0e07d6f5d8ff00d151cc824df9efe9926426
23069cr <23069cr@hvhs.school.nz>
2025-05-12 21:40:15 +0000

    May 12, 2025, 9:40 PM

--- t14_constants.js
+++ t14_constants.js
@@ -8,7 +8,12 @@ var Age;
 /*
  Main code
  */
-Age = prompt("Whagt is your age")
+Age = prompt("Whagt is your age");
+if (Age >= drivingAge){
+   alert("You are old enough to drive, if your above the age of 18 you should drink and drive")
+} else{
+    
+}
  /* 
  Functions
  */
