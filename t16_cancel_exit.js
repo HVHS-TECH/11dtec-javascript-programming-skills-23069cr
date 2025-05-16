ab0f163216b5bdae655b692fa73668bacf60c812
23069cr <23069cr@hvhs.school.nz>
2025-05-16 02:41:40 +0000

    May 16, 2025, 2:41 AM

---
+++ t16_cancel_exit.js
@@ -0,0 +1,28 @@
+/*
+t13_validation.js Coby 13/05/2025, Validation JavaScript
+*/
+
+//Variables
+var Age
+/*
+ Main code
+ */
+Age = prompt("How old are you?");
+
+ if(Age == null || Age=="" || Age == " "){
+    prompt("INVALID! YOU MUST ENTER A NUMBER FOR YOUR AGE!!!!")
+   } else {
+      return;
+ }
+ if(Age < 0 ||  Age > 130){
+    Age = prompt("YOU HAVEN'T LEARNED!!! YOUR AGE CANNOT BE IN THE NEGATIVES AND YOU'RE DEFINITLY NOT OVER 130 YEARS OLD!!!!!!!!!!!!!!") 
+ } else {
+ return;
+ }
+ if(Age > 0 ||  Age < 130){
+    Age = alert("Thank you for your service =)")
+    return;
+ }
+ /* 
+ Functions
+ */
